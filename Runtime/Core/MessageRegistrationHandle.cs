9bca77195ac80c9d4a8e9d5b0eb96b06a00e48df
wallstop <wallstop@wallstopstudios.com>
2024-01-06 12:06:22 -0800

    Add comparable id to message registration handles

--- Runtime/Core/MessageRegistrationHandle.cs
+++ Runtime/Core/MessageRegistrationHandle.cs
@@ -1,20 +1,28 @@
 ﻿namespace DxMessaging.Core
 {
     using System;
+    using System.Threading;
+
+    public static class MessageRegistrationHandleData
+    {
+        public static long IdCount = 0;
+    }
 
     public readonly struct MessageRegistrationHandle : IEquatable<MessageRegistrationHandle>, IComparable<MessageRegistrationHandle>
     {
         private readonly Guid _handle;
+        private readonly long _id;
         private readonly int _hashCode;
 
         public static MessageRegistrationHandle CreateMessageRegistrationHandle()
         {
-            return new MessageRegistrationHandle(Guid.NewGuid());
+            return new MessageRegistrationHandle(Guid.NewGuid(), Interlocked.Increment(ref MessageRegistrationHandleData.IdCount));
         }
 
-        private MessageRegistrationHandle(Guid handle)
+        private MessageRegistrationHandle(Guid handle, long id)
         {
             _handle = handle;
+            _id = id;
             _hashCode = _handle.GetHashCode();
         }
 
@@ -30,12 +38,12 @@
 
         public bool Equals(MessageRegistrationHandle other)
         {
-            return _handle.Equals(other._handle);
+            return _id == other._id && _handle.Equals(other._handle);
         }
 
         public int CompareTo(MessageRegistrationHandle other)
         {
-            return _handle.CompareTo(other._handle);
+            return _id.CompareTo(other._id);
         }
     }
 }
---
+++ Tests/Runtime/Core/RegistrationTests.cs
@@ -0,0 +1,40 @@
+﻿namespace DxMessaging.Tests.Runtime.Core
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using DxMessaging.Core;
+    using NUnit.Framework;
+    using Scripts.Components;
+    using Scripts.Messages;
+    using UnityEngine;
+    using UnityEngine.TestTools;
+
+    public sealed class RegistrationTests : MessagingTestBase
+    {
+
+        [UnityTest]
+        public IEnumerator UntargetedNormal()
+        {
+            GameObject test = new(nameof(UntargetedNormal), typeof(EmptyMessageAwareComponent));
+            _spawned.Add(test);
+            EmptyMessageAwareComponent component = test.GetComponent<EmptyMessageAwareComponent>();
+            MessageRegistrationToken token = GetToken(component);
+
+            List<MessageRegistrationHandle> handles = new(_numRegistrations);
+            int count = 0;
+
+            void Handle(SimpleUntargetedMessage message)
+            {
+                ++count;
+            }
+
+            for (int i = 0; i < _numRegistrations; ++i)
+            {
+                var handle = token.RegisterUntargeted<SimpleUntargetedMessage>(Handle);
+                Assert.IsFalse(handles.Contains(handle));
+            }
+
+            yield break;
+        }
+    }
+}
