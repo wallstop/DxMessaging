6917e317d1ec0fbbb1bfe70c8173e4994bf237f9
wallstop <wallstop@wallstopstudios.com>
2024-01-06 14:00:08 -0800

    Update performance tests

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 Game engine agnostic robust, synchronous pub/sub C# messaging solution, mostly geared towards Unity and XNA/Monogame. See [this recorded talk](https://fathom.video/share/qjs8pn1MAwGb-yTAaW5WpZWdjzxcdwFR).
 
 # Benchmarks
-DxMessaging is currently a bit slower (2-4x) than Unity's built in messaging solution (when running in Unity). [Source](./Tests/Runtime/Core/PerformanceTests.cs).
+DxMessaging is currently a bit slower (2-3x) than Unity's built in messaging solution (when running in Unity). [Source](./Tests/Runtime/Core/PerformanceTests.cs).
 | Message Tech | Operations / Second |
 | ------------ | ------------------- |
-| Unity | 1,966,429 |
-| DxMessaging (GameObject) - Normal | 408,558 |
-| DxMessaging (GameObject) - No-Copy | 414,814 |
-| DxMessaging (Component) - No-Copy | 451,439 |
-| DxMessaging (Untargeted) - No-Copy | 952,495 |
+| Unity | 1,908,130 |
+| DxMessaging (GameObject) - Normal | 592,492 |
+| DxMessaging (GameObject) - No-Copy | 579,615 |
+| DxMessaging (Component) - No-Copy | 606,212 |
+| DxMessaging (Untargeted) - No-Copy | 1,045,220 |
 
 # Functionality
 While not as fast, DxMessaging offers *additional functionality* as compared to Unity's messaging solution.
--- Tests/Runtime/Benchmarks/PerformanceTests.cs
+++ Tests/Runtime/Benchmarks/PerformanceTests.cs
@@ -4,40 +4,54 @@
     using System.Collections;
     using System.Diagnostics;
     using Core;
+    using DxMessaging.Core;
     using DxMessaging.Core.Extensions;
+    using NUnit.Framework;
     using Scripts.Components;
     using Scripts.Messages;
     using UnityEngine;
     using UnityEngine.TestTools;
     using Debug = UnityEngine.Debug;
+    using Object = UnityEngine.Object;
 
     public sealed class PerformanceTests : MessagingTestBase
     {
+        [SetUp]
+        public override void Setup()
+        {
+            base.Setup();
+            MessagingDebug.LogFunction = (_1, _2) => { };
+        }
+
         [UnityTest]
         public IEnumerator Benchmark()
         {
-            // Add some components in for good measure
-            GameObject target = new(
-                nameof(Benchmark), typeof(SimpleMessageAwareComponent), typeof(SpriteRenderer),
-                typeof(Rigidbody2D), typeof(CircleCollider2D), typeof(LineRenderer));
-            _spawned.Add(target);
-
-            SimpleMessageAwareComponent component = target.GetComponent<SimpleMessageAwareComponent>();
-
             TimeSpan timeout = TimeSpan.FromSeconds(1);
+
             Debug.Log("| Message Tech | Operations / Second |");
             Debug.Log("| ------------ | ------------------- |");
 
             ComplexTargetedMessage message = new(Guid.NewGuid());
             Stopwatch timer = Stopwatch.StartNew();
-            Unity(timer, timeout, target, component, message);
-            NormalGameObject(timer, timeout, component, message);
-            NoCopyGameObject(timer, timeout, component, message);
-            NoCopyComponent(timer, timeout, component, message);
 
+            RunTest(component => Unity(timer, timeout, component.gameObject, message));
+            RunTest(component => NormalGameObject(timer, timeout, component, message));
+            RunTest(component => NoCopyGameObject(timer, timeout, component, message));
+            RunTest(component => NoCopyComponent(timer, timeout, component, message));
+            
             SimpleUntargetedMessage untargetedMessage = new();
-            NoCopyUntargeted(timer, timeout, component, untargetedMessage);
+            RunTest(component => NoCopyUntargeted(timer, timeout, component, untargetedMessage));
             yield break;
+        }
+
+        private GameObject CreateGameObject()
+        {
+            GameObject target = new(
+                nameof(Benchmark), typeof(EmptyMessageAwareComponent), typeof(SpriteRenderer),
+                typeof(Rigidbody2D), typeof(CircleCollider2D), typeof(LineRenderer));
+            _spawned.Add(target);
+
+            return target;
         }
 
         private void DisplayCount(string testName, int count, TimeSpan timeout)
@@ -45,9 +59,24 @@
             Debug.Log($"| {testName} | {Math.Floor(count / timeout.TotalSeconds):N0} |");
         }
 
-        private void Unity(Stopwatch timer, TimeSpan timeout, GameObject target, SimpleMessageAwareComponent component, ComplexTargetedMessage message)
+        private void RunTest(Action<EmptyMessageAwareComponent> test)
+        {
+            GameObject go = CreateGameObject();
+            try
+            {
+                test(go.GetComponent<EmptyMessageAwareComponent>());
+            }
+            finally
+            {
+                _spawned.Remove(go);
+                Object.Destroy(go);
+            }
+        }
+
+        private void Unity(Stopwatch timer, TimeSpan timeout, GameObject target,  ComplexTargetedMessage message)
         {
             int count = 0;
+            var component = target.AddComponent<SimpleMessageAwareComponent>();
             component.slowComplexTargetedHandler = () => ++count;
             timer.Restart();
             do
@@ -58,30 +87,39 @@
             DisplayCount("Unity", count, timeout);
         }
 
-        private void NormalGameObject(Stopwatch timer, TimeSpan timeout, SimpleMessageAwareComponent component, ComplexTargetedMessage message)
+        private void NormalGameObject(Stopwatch timer, TimeSpan timeout, EmptyMessageAwareComponent component, ComplexTargetedMessage message)
         {
             int count = 0;
-            component.slowComplexTargetedHandler = () => ++count;
-            component.complexTargetedHandler = null;
-            component.SlowComplexTargetingEnabled = true;
-            component.FastComplexTargetingEnabled = false;
+            var token = GetToken(component);
 
+            void Handle(ComplexTargetedMessage _)
+            {
+                ++count;
+            }
+
+            GameObject go = component.gameObject;
+            token.RegisterGameObjectTargeted<ComplexTargetedMessage>(go, Handle);
             timer.Restart();
             do
             {
-                message.EmitGameObjectTargeted(component.gameObject);
+                message.EmitGameObjectTargeted(go);
             }
             while (timer.Elapsed < timeout);
             DisplayCount("DxMessaging (GameObject) - Normal", count, timeout);
         }
 
-        private void NoCopyGameObject(Stopwatch timer, TimeSpan timeout, SimpleMessageAwareComponent component, ComplexTargetedMessage message)
+        private void NoCopyGameObject(Stopwatch timer, TimeSpan timeout, EmptyMessageAwareComponent component, ComplexTargetedMessage message)
         {
             int count = 0;
-            component.slowComplexTargetedHandler = null;
-            component.complexTargetedHandler = () => ++count;
-            component.SlowComplexTargetingEnabled = false;
-            component.FastComplexTargetingEnabled = true;
+            var token = GetToken(component);
+
+            void Handle(ref ComplexTargetedMessage _)
+            {
+                ++count;
+            }
+
+            GameObject go = component.gameObject;
+            token.RegisterGameObjectTargeted<ComplexTargetedMessage>(go, Handle);
 
             timer.Restart();
             do
@@ -92,14 +130,17 @@
             DisplayCount("DxMessaging (GameObject) - No-Copy", count, timeout);
         }
 
-        private void NoCopyComponent(Stopwatch timer, TimeSpan timeout, SimpleMessageAwareComponent component, ComplexTargetedMessage message)
+        private void NoCopyComponent(Stopwatch timer, TimeSpan timeout, EmptyMessageAwareComponent component, ComplexTargetedMessage message)
         {
             int count = 0;
-            component.slowComplexTargetedHandler = null;
-            component.complexTargetedHandler = null;
-            component.complexComponentTargetedHandler = () => ++count;
-            component.SlowComplexTargetingEnabled = false;
-            component.FastComplexTargetingEnabled = false;
+            var token = GetToken(component);
+
+            void Handle(ref ComplexTargetedMessage _)
+            {
+                ++count;
+            }
+
+            token.RegisterComponentTargeted<ComplexTargetedMessage>(component, Handle);
 
             timer.Restart();
             do
@@ -110,10 +151,17 @@
             DisplayCount("DxMessaging (Component) - No-Copy", count, timeout);
         }
 
-        private void NoCopyUntargeted(Stopwatch timer, TimeSpan timeout, SimpleMessageAwareComponent component, SimpleUntargetedMessage message)
+        private void NoCopyUntargeted(Stopwatch timer, TimeSpan timeout, EmptyMessageAwareComponent component, SimpleUntargetedMessage message)
         {
             int count = 0;
-            component.untargetedHandler = () => ++count;
+            var token = GetToken(component);
+
+            void Handle(ref SimpleUntargetedMessage _)
+            {
+                ++count;
+            }
+
+            token.RegisterUntargeted<SimpleUntargetedMessage>(Handle);
 
             timer.Restart();
             do
