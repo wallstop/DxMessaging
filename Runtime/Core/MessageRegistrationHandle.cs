126f4a8104d51c6975f748c69b582cbc2642405a
wallstop <wallstop@wallstopstudios.com>
2024-01-06 12:23:38 -0800

    Add UntargetedRegistration tests

--- Runtime/Core/MessageRegistrationHandle.cs
+++ Runtime/Core/MessageRegistrationHandle.cs
@@ -3,20 +3,17 @@
     using System;
     using System.Threading;
 
-    public static class MessageRegistrationHandleData
-    {
-        public static long IdCount = 0;
-    }
-
     public readonly struct MessageRegistrationHandle : IEquatable<MessageRegistrationHandle>, IComparable<MessageRegistrationHandle>
     {
+        private static long StaticIdCount;
+
         private readonly Guid _handle;
         private readonly long _id;
         private readonly int _hashCode;
 
         public static MessageRegistrationHandle CreateMessageRegistrationHandle()
         {
-            return new MessageRegistrationHandle(Guid.NewGuid(), Interlocked.Increment(ref MessageRegistrationHandleData.IdCount));
+            return new MessageRegistrationHandle(Guid.NewGuid(), Interlocked.Increment(ref StaticIdCount));
         }
 
         private MessageRegistrationHandle(Guid handle, long id)
@@ -44,6 +41,15 @@
         public int CompareTo(MessageRegistrationHandle other)
         {
             return _id.CompareTo(other._id);
+        }
+
+        public override string ToString()
+        {
+            return new
+            {
+                Handle = _handle.ToString(),
+                Id = _id,
+            }.ToString();
         }
     }
 }
--- Tests/Runtime/Core/RegistrationTests.cs
+++ Tests/Runtime/Core/RegistrationTests.cs
@@ -1,8 +1,11 @@
 ﻿namespace DxMessaging.Tests.Runtime.Core
 {
+    using System;
     using System.Collections;
     using System.Collections.Generic;
+    using System.Linq;
     using DxMessaging.Core;
+    using DxMessaging.Core.Extensions;
     using NUnit.Framework;
     using Scripts.Components;
     using Scripts.Messages;
@@ -11,30 +14,165 @@
 
     public sealed class RegistrationTests : MessagingTestBase
     {
-
         [UnityTest]
         public IEnumerator UntargetedNormal()
+        {
+            int count = 0;
+            void Handle(SimpleUntargetedMessage message)
+            {
+                ++count;
+            }
+
+            SimpleUntargetedMessage message = new();
+            RunRegistrationTest(
+                token => token.RegisterUntargeted<SimpleUntargetedMessage>(Handle), i =>
+                {
+                    Assert.AreEqual(i, count);
+                    message.EmitUntargeted();
+                }, i =>
+                {
+                    Assert.AreEqual(i, count);
+                    message.EmitUntargeted();
+                    Assert.AreEqual(i + 1, count);
+                }, () =>
+                {
+                    message.EmitUntargeted();
+                    Assert.AreEqual(0, count);
+                }, () =>
+                {
+                    count = 0;
+                });
+            yield break;
+        }
+
+        [UnityTest]
+        public IEnumerator UntargetedNoCopy()
+        {
+            int count = 0;
+            void Handle(ref SimpleUntargetedMessage message)
+            {
+                ++count;
+            }
+
+            SimpleUntargetedMessage message = new();
+            RunRegistrationTest(
+                token => token.RegisterUntargeted<SimpleUntargetedMessage>(Handle), i =>
+                {
+                    Assert.AreEqual(i, count);
+                    message.EmitUntargeted();
+                }, i =>
+                {
+                    Assert.AreEqual(i, count);
+                    message.EmitUntargeted();
+                    Assert.AreEqual(i + 1, count);
+                }, () =>
+                {
+                    message.EmitUntargeted();
+                    Assert.AreEqual(0, count);
+                }, () =>
+                {
+                    count = 0;
+                });
+            yield break;
+        }
+
+        [UnityTest]
+        public IEnumerator UntargetedPostProcessor()
+        {
+            int count = 0;
+            void Handle(ref SimpleUntargetedMessage message)
+            {
+                ++count;
+            }
+
+            SimpleUntargetedMessage message = new();
+            RunRegistrationTest(
+                token => token.RegisterUntargetedPostProcessor<SimpleUntargetedMessage>(Handle), i =>
+                {
+                    Assert.AreEqual(i, count);
+                    message.EmitUntargeted();
+                }, i =>
+                {
+                    Assert.AreEqual(i, count);
+                    message.EmitUntargeted();
+                    Assert.AreEqual(i + 1, count);
+                }, () =>
+                {
+                    message.EmitUntargeted();
+                    Assert.AreEqual(0, count);
+                }, () =>
+                {
+                    count = 0;
+                });
+            yield break;
+        }
+
+        [UnityTest]
+        public IEnumerator UntargetedInterceptor()
+        {
+            int count = 0;
+            bool Intercept(ref SimpleUntargetedMessage message)
+            {
+                ++count;
+                return true;
+            }
+
+            SimpleUntargetedMessage message = new();
+            RunRegistrationTest(
+                token => token.RegisterUntargetedInterceptor<SimpleUntargetedMessage>(Intercept), i =>
+                {
+                    Assert.AreEqual(i, count);
+                    message.EmitUntargeted();
+                }, i =>
+                {
+                    Assert.AreEqual(i, count);
+                    message.EmitUntargeted();
+                    Assert.AreEqual(i + 1, count);
+                }, () =>
+                {
+                    message.EmitUntargeted();
+                    Assert.AreEqual(0, count);
+                }, () =>
+                {
+                    count = 0;
+                });
+            yield break;
+        }
+
+        private void RunRegistrationTest(Func<MessageRegistrationToken, MessageRegistrationHandle> registration, Action<int> normalAssert, Action<int> removalAssert, Action finalAssert, Action reset)
         {
             GameObject test = new(nameof(UntargetedNormal), typeof(EmptyMessageAwareComponent));
             _spawned.Add(test);
             EmptyMessageAwareComponent component = test.GetComponent<EmptyMessageAwareComponent>();
             MessageRegistrationToken token = GetToken(component);
 
-            List<MessageRegistrationHandle> handles = new(_numRegistrations);
-            int count = 0;
-
-            void Handle(SimpleUntargetedMessage message)
-            {
-                ++count;
-            }
+            HashSet<MessageRegistrationHandle> handles = new(_numRegistrations);
 
             for (int i = 0; i < _numRegistrations; ++i)
             {
-                var handle = token.RegisterUntargeted<SimpleUntargetedMessage>(Handle);
-                Assert.IsFalse(handles.Contains(handle));
+                MessageRegistrationHandle handle = registration(token);
+                bool neverSeen = handles.Add(handle);
+                Assert.IsTrue(neverSeen, "Handle {0} at count {1} was a duplicate.", handle, neverSeen);
             }
 
-            yield break;
+            for (int i = 0; i < 100; ++i)
+            {
+                normalAssert(i);
+            }
+
+            reset();
+            int expected = 0;
+            foreach (MessageRegistrationHandle handle in handles.OrderBy(_ => _random.Next()).ToList())
+            {
+                removalAssert(expected++);
+                token.RemoveRegistration(handle);
+            }
+
+            reset();
+            for (int i = 0; i < 100; ++i)
+            {
+                finalAssert();
+            }
         }
     }
 }
