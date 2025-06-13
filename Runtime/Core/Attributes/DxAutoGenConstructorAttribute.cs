ffc8e8ea9f72976d2d66bde6c937e1971a6a3f21
wallstop <wallstop@wallstopstudios.com>
2025-06-12 18:09:13 -0700

    Initial code gen

---
+++ Runtime/Core/Attributes/DxAutoGenConstructorAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace DxMessaging.Core.Attributes
+{
+    using System;
+
+    [AttributeUsage(
+        AttributeTargets.Class | AttributeTargets.Struct,
+        Inherited = false,
+        AllowMultiple = false
+    )]
+    public sealed class DxAutoGenConstructorAttribute : Attribute
+    {
+        public DxAutoGenConstructorAttribute() { }
+    }
+}
